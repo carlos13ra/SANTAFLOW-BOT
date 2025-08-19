6ca4668bda617809022203dce93f783587f4fcbe
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-08-19 11:24:43 -0500

    Update descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -81,7 +81,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
       contextInfo: {
         externalAdReply: {
           title: title,
-          body: '⚽ RIN ITOSHI - IA 🌀',
+          body: '⚽ SANTAFLOW - IA 🌀',
           mediaType: 1,
           thumbnail: thumb,
           mediaUrl: url,
