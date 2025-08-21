4b6937ebcb1ea609228f278d1af32e0a603d481a
the-27 <blackoficial2025@gmail.com>
2025-08-21 13:32:39 -0500

    update

--- plugins/descargas-spotify.js
+++ plugins/descargas-spotify.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, text }) => {
     await conn.sendMessage(m.chat, {
       audio: { url: data.download },
       mimetype: 'audio/mpeg',
-      ptt: true,
+      ptt: false,
       fileName: `${data.title}.mp3`,
       contextInfo: {
         externalAdReply: {
