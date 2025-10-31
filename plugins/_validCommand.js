8ba3c608023e284353c8207c46524bf438282694
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-10-30 21:07:21 -0500

    Update _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -1,10 +1,32 @@
+import fetch from 'node-fetch';
+
 export async function before(m, { conn }) {
   if (!m.text || !global.prefix.test(m.text)) return;
 
   const usedPrefix = global.prefix.exec(m.text)[0];
   const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
   
-  const fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=51919199620:51919199620\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net"}
+  const thumbRes = await fetch("https://files.catbox.moe/xydiwe.jpg");
+  const thumbBuffer = await thumbRes.buffer();
+  const fkontak = {
+        key: {
+           participants: "0@s.whatsapp.net",
+           remoteJid: "status@broadcast",
+           fromMe: false,
+           id: "Halo"
+        },
+        message: {
+            locationMessage: {
+                name: `*̥₊🎧 𝑺𝒂𝒏𝒕𝒂𝒇𝒍𝒐𝒘 - 𝑩𝒐𝒕 | © 𝘣𝘺 ᴄᴀʀʟᴏs ʀᴀᴍɪʀᴇᴢ ◌🥭`,
+                jpegThumbnail: thumbBuffer
+            }
+        },
+        participant: "0@s.whatsapp.net"
+  };
+  const channelRD = { 
+    id: '120363401008003732@newsletter', 
+    name: '🎧 𝐒𝐀𝐍𝐓𝐀𝐅𝐋𝐎𝐖 - 𝐁𝐎𝐓 | 𝐂𝐇𝐀𝐍𝐍𝐄𝐋 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 💫'
+  };
 
   if (!command || command === 'bot') return;
 
@@ -21,28 +43,37 @@ export async function before(m, { conn }) {
     let user = global.db.data.users[m.sender];
 
     if (chat?.isBanned) {
-      const avisoDesactivado = `╭─⭑❨ 🔒 𝐁𝐎𝐓 𝐃𝐄𝐒𝐀𝐂𝐓𝐈𝐕𝐀𝐃𝐎 ❩⭑─╮
-│ 🚫 *${bot}* 𝑒𝑠𝑡𝑎 *desactivado* 𝑒𝑛 𝑒𝑠𝑡𝑒 𝑔𝑟𝑢𝑝𝑜.
-│ 🎮 𝑆𝑖𝑛 𝑒𝑙 𝑠𝑖𝑠𝑡𝑒𝑚𝑎 𝑎𝑐𝑡𝑖𝑣𝑜, 𝑛𝑜 𝑝𝑢𝑒𝑑𝑒𝑠 𝑢𝑠𝑎𝑟 𝑐𝑜𝑚𝑎𝑛𝑑𝑜𝑠.
-│ 🧃 𝐒𝐨𝐥𝐨 𝐮𝐧 *administrador* 𝐩𝐮𝐞𝐝𝐞 𝐯𝐨𝐥𝐯𝐞𝐫 𝐚 𝐚𝐜𝐭𝐢𝐯𝐚𝐫𝐥𝐨.
-│ ✅ 𝐔𝐬𝐚: *${usedPrefix}bot on*
-╰────────────────────────╯`;
+      const avisoDesactivado = `╭─⭑༺ 🔒 𝐁𝐎𝐓 𝐃𝐄𝐒𝐀𝐂𝐓𝐈𝐕𝐀𝐃𝐎 ༻⭑─╮
+│ ✖️  *${bot}* está en *modo inactivo*.  
+│ 💬  Los comandos están *bloqueados*.  
+│ 📌  Solo un *administrador* puede  
+│      volver a *activarlo*.  
+│  
+│ 💠  Actívalo con: *${usedPrefix}bot on*  
+╰───────────────────────⬯`;
 
       await conn.sendMessage(m.chat, {
-        text: avisoDesactivado,
-        mentions: [m.sender],
-        contextInfo: {
-          externalAdReply: {
-            title: dev,
-            body: packname,
-            thumbnailUrl: banner,
-            sourceUrl: 'https://github.com/carlos13ra',
-            mediaType: 1,
-            renderLargerThumbnail: true
-          }
-        }
-      }, { quoted: fkontak });
-      return;
+      text: avisoDesactivado,
+      mentions: [m.sender],
+      contextInfo: {
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: channelRD.id,
+          serverMessageId: '',
+          newsletterName: channelRD.name
+        },
+        externalAdReply: {
+          title: '◌*̥₊ 🎧 𝐒𝐀𝐍𝐓𝐀𝐅𝐋𝐎𝐖 - 𝐁𝐎𝐓 ◌📀༉',
+          body: '',
+          thumbnailUrl: 'https://files.catbox.moe/6fj9u7.jpg',
+          sourceUrl: '',
+          mediaType: 1,
+          renderLargerThumbnail: true
+        },
+        mentionedJid: null
+      }
+    }, { quoted: fkontak });
+    return;
     }
 
     if (!user.commands) user.commands = 0;
@@ -52,45 +83,48 @@ export async function before(m, { conn }) {
 
   //await m.react('💔');
   const mensajesNoEncontrado = [
-    `╭━〔 🚫 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐈𝐍𝐄𝐗𝐈𝐒𝐓𝐄𝐍𝐓𝐄 〕━⬣
-┃ ✦ El comando *"${command}"* no se reconoce.
-┃ ✦ Menú disponible: *${usedPrefix}menu*
-╰━━━━━━━━━━━━━━━━━━━━━⬣`,
+    `> ⌗ El comando *"${command}"* no se reconoce.
+> ⌗ Menú disponible: *${usedPrefix}menu*`,
 
-    `─❖〔 🌀 𝐄𝐑𝐑𝐎𝐑 𝐃𝐄 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 〕❖─
- ✧ *"${command}"* no forma parte del sistema.
+    `✧ *"${command}"* no forma parte del sistema.
  ✧ Consulta: *${usedPrefix}menu*`,
 
-    `❀ 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐍𝐎 𝐄𝐍𝐂𝐎𝐍𝐓𝐑𝐀𝐃𝐎 ❀
-🖋️ *"${command}"* no está registrado.
-📜 Usa *${usedPrefix}menu* para ver opciones.`,
+    `❐ *"${command}"* no está registrado.
+❐ Usa *${usedPrefix}menu* para ver opciones.`,
 
-    `🍂 El comando *"${command}"* no existe.
-📖 Consulta el menú: *${usedPrefix}menu*`,
+    `🎵 El comando *"${command}"* no existe.
+🌤️ Consulta el menú: *${usedPrefix}menu*`,
 
-    `─〔 ⛔ 𝐂𝐎𝐌𝐀𝐍𝐃𝐎 𝐃𝐄𝐒𝐂𝐎𝐍𝐎𝐂𝐈𝐃𝐎 〕─
-🪶 *"${command}"* no está disponible.
-📂 Menú: *${usedPrefix}menu*`,
+    `🎤 *"${command}"* no está disponible.
+🔥 Menú: *${usedPrefix}menu*`,
 
-    `❌ Comando: *"${command}"* inválido.
-📜 Usa: *${usedPrefix}menu* para ver todos los comandos disponibles.`
+    `📌 Comando: *"${command}"* inválido.
+💫 Usa: *${usedPrefix}menu* para ver todos los comandos disponibles.`
   ];
 
   const texto = mensajesNoEncontrado[Math.floor(Math.random() * mensajesNoEncontrado.length)];
-  const imgurl = icono;
+  const thumb = 'https://files.catbox.moe/6fj9u7.jpg';
 
+  
   await conn.sendMessage(m.chat, {
     text: texto,
     mentions: [m.sender],
     contextInfo: {
+      isForwarded: true,
+      forwardedNewsletterMessageInfo: {
+        newsletterJid: channelRD.id,
+        serverMessageId: '',
+        newsletterName: channelRD.name
+      },
       externalAdReply: {
-        title: dev,
-        body: packname,
-        thumbnailUrl: imgurl,
-        sourceUrl: 'https://instagram.com',
+        title: ' 🎃 𝐒𝐀𝐍𝐓𝐀𝐅𝐋𝐎𝐖 - 𝐁𝐎𝐓 📀',
+        body: '',
+        thumbnailUrl: thumb,
+        sourceUrl: '',
         mediaType: 1,
         renderLargerThumbnail: true
-      }
+      },
+     mentionedJid: null
     }
   }, { quoted: fkontak });
 }
