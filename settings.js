2271154e6b2af596d16b9873d3e7dc274cb5faba
Carlos Ramírez Villanueva <carlosramirezvillanueva30@gmail.com>
2025-10-30 21:18:01 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -83,9 +83,9 @@ global.correo = 'thekingdestroy507@gmail.com'
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
 global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
 global.ch = {
-ch1: '120363401008003732@newsletter',
-ch2: "120363401008003732@newsletter",
-ch3: "120363401008003732@newsletter"
+ch1: "120363402079893698@newsletter",
+ch2: "120363402079893698@newsletter",
+ch3: "120363402079893698@newsletter"
 }
 global.multiplier = 60
 
